85a808951befdd582a8c14ac64b214fa520167c8
Jonathan Junior <jonathanjroliveira07@gmail.com>
2024-10-04 22:55:38 -0300

    curso

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
     this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
   }
 
   initialize() {
@@ -20,10 +21,26 @@ class CalcController {
     }, 1000);
   }
 
+  initButtonsEvents() {
+
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");    
+
+    buttons.forEach((btn, index) => {
+      
+      btn.addEventListener('click', e=> {
+
+        console.log(btn.className.baseVal.replace("btn-", ""));
+        
+  
+      });
+    })
+
+  }
+
   setDisplayDateTime() {
     this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
         day: "2-digit",
-        month: "long",
+        month: "short",
         year: "numeric"
 
     });
